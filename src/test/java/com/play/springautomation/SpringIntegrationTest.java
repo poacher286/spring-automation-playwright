0f6cfaaf1f345d53db5ba2e26c8e56f2bd6cef09
Buland Sahu <poacher@Bulands-MacBook-Pro.local>
2021-05-11 01:35:07 +0530

    Fixed CLI integration test

--- src/test/java/com/play/springautomation/SpringIntegrationTest.java
+++ src/test/java/com/play/springautomation/SpringIntegrationTest.java
@@ -2,9 +2,11 @@ package com.play.springautomation;
 
 import io.cucumber.spring.CucumberContextConfiguration;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ContextConfiguration;
 
+@ContextConfiguration
 @CucumberContextConfiguration
 @SpringBootTest(classes = SpringAutomationApplication.class)
-public class SpringIntegrationTest {
+public abstract class SpringIntegrationTest {
 
 }
